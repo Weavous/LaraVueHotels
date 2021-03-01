cf2f404bc9369a29c4b10f0e9df3a4406d0031f0
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 16:30:54 -0300

    Addresses.vue

---
+++ frontend/src/components/Addresses.vue
@@ -0,0 +1,28 @@
+<template>
+  <select class="form-control" required>
+    <option v-for="(address, key) in this.data" v-bind:value="address.id" :key="key">
+      {{ `${address.state} - ${address.city} - ${address.street}` }}
+    </option>
+  </select>
+</template>
+
+<script>
+import addresses from "../services/addresses.js";
+
+export default {
+  name: "Addresses",
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    addresses.index().then((response) => {
+      this.data = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
