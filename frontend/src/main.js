c842a0caa333f81d80450c80d214de5909aecf9a
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 15:24:57 -0300

    main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import Home from './pages/Home.vue';
+import Register from './pages/Register.vue';
+import Pricing from './pages/Pricing.vue';
+import About from './pages/About.vue';
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/register', component: Register },
+    { path: '/pricing', component: Pricing },
+    { path: '/about', component: About },
+  ]
+});
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
-})
+});
