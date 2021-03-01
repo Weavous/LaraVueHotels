ee933a31c403b326e9b9d70915f98bd3e04c95e8
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 20:15:11 -0300

    properties.js

--- frontend/src/services/properties.js
+++ frontend/src/services/properties.js
@@ -1,6 +1,7 @@
 import { http } from './config.js';
 
 export default {
-    index: () => { return http.get('/properties') },
-    store: (data) => { return http.post('/properties', data) }
+    index: () => { return http.get(`/properties`) },
+    store: (data) => { return http.post(`/properties`, data) },
+    show: (id) => { return http.get(`/properties/${id}`) }
 }
