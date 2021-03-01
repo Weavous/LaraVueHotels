65b6d7472d1b336e54acbd2fa89fe14ea7abbc31
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 18:22:55 -0300

    Carousel.vue

---
+++ frontend/src/components/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../assets/carousel/9SpjD3M5PqOxoWWAdPzByQ==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/carousel/69fkgMaR17dKzlFZk2mISw==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/carousel/dOenEgVlg2jdrtMVmEcpJw==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/carousel/p6Nuvr9O2Ehn262106yIYA==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/carousel/PAFJS+4Qzmpcq7USrC87Ug==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/carousel/RPykgNArgIVEQ4XlogcSOg==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/carousel/ScPtazXyWQz2b9u94Loiw==.jpg" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
