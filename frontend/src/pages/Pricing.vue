ae37e79bce86d29063a8f0087f47988a283d42e0
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 15:21:24 -0300

    Pricing.vue

---
+++ frontend/src/pages/Pricing.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container p-3">
+    <div class="row">
+      <div v-for="(property, key) in data" class="col-md-4" :key="key">
+        <Card :data="{ property }"></Card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "../components/Card.vue";
+
+import properties from "../services/properties.js";
+
+export default {
+  name: "Pricing",
+  components: { Card },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    properties.all().then((response) => {
+      this.data = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
