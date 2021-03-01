dbc615f4f9b34a95bcd64a412ea6b5186f724db4
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 19:30:28 -0300

    Card.vue

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="card">
-    <img src="https://source.unsplash.com/user/erondu/1600x900" class="card-img-top" alt="#"/>
+    <img src="https://source.unsplash.com/user/erondu/1600x900" class="card-img-top" alt="Card" />
     <div class="card-body">
       <h5 class="card-title">{{ this.data.property.title }}</h5>
-      <p class="card-text">{{ this.data.property.description }}</p>
+      <p class="card-text">{{ this.data.property.description | str32 }}</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated {{ new Date(this.data.property.updated_at).toDateString() }}</small>
+      <div class="row">
+        <div class="col-10 col-md-10">
+          <small class="text-muted">Last updated {{ new Date(this.data.property.updated_at).toDateString() }}</small>
+        </div>
+        <div class="col-2 col-md-2">
+          <i class="bi bi-blockquote-left"></i>
+          <i class="bi bi-grid-3x3-gap-fill" title="Edit this record?"></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +28,11 @@ export default {
       type: Object,
     },
   },
+  filters: {
+    str32(text){
+      return text.substring(0, 32);
+    }
+  }
 };
 </script>
 
