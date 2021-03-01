d9cf464527d5d5338abab3423d0f5dbc0ac48803
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 15:20:58 -0300

    Menu.vue

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,20 +1,23 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light py-0">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="../assets/logo.png" alt="Logo" /> LaraVueHotels</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <router-link class="navbar-brand" to="/"><img src="../assets/logo.png" alt="Logo" /> LaraVueHotels</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
+      <div class="collapse navbar-collapse navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
+            <router-link class="nav-link" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <router-link class="nav-link" to="/register">Register</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <router-link class="nav-link" to="/pricing">Pricing</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
           </li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
@@ -25,10 +28,12 @@
             <div class="nav-link">
               <div class="collapse navbar-collapse">
                 <div class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-box-arrow-right"></i> Login / Sign Up</a>
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-box-arrow-right"></i> Login / Sign Up</a>
 
-                  <ul class="dropdown-menu dropdown-menu-light bg-light" aria-labelledby="navbarLightDropdownMenuLink">
-                    <li><a class="dropdown-item border-bottom" href="#">Login</a></li>
+                  <ul class="dropdown-menu dropdown-menu-light bg-light">
+                    <li>
+                      <a class="dropdown-item border-bottom" href="#">Login</a>
+                    </li>
                     <li><a class="dropdown-item" href="#">Sign Up</a></li>
                   </ul>
                 </div>
