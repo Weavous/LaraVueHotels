b2597e0fe206fb492f46ee7edd25d8ee7670c21f
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 15:17:09 -0300

    Card.vue

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <img src="https://source.unsplash.com/user/erondu/1600x900" class="card-img-top" alt="#"/>
+    <div class="card-body">
+      <h5 class="card-title">{{ this.data.property.title }}</h5>
+      <p class="card-text">{{ this.data.property.description }}</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated {{ new Date(this.data.property.updated_at).toDateString() }}</small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    data: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
