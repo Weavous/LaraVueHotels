08d4cd32878810b358bc4e5cb996d3e8ba8e8f70
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-02 12:18:02 -0300

    GeolocatorTest.php

--- backend/tests/Unit/GeolocatorTest.php
+++ backend/tests/Unit/GeolocatorTest.php
@@ -7,15 +7,28 @@ use PHPUnit\Framework\TestCase;
 class GeolocatorTest extends TestCase
 {
     /**
-     * Tests the calculation of the distance.
+     * Tests the calculation of the positives distance.
      *
      * @return void
      */
-    public function test_distance_calc()
+    public function test_positive_distance_calc()
     {
         $x = \App\Domain\Math\GPS::create(23.51014, 46.64247);
         $y = \App\Domain\Math\GPS::create(23.50831, 46.63139);
 
         $this->assertEquals(\App\Domain\Math\Geolocator::create($x, $y)->arc2dist(), 1247.89);
+    }
+
+    /**
+     * Tests the calculation of the negatives distance.
+     *
+     * @return void
+     */
+    public function test_negative_distance_calc()
+    {
+        $x = \App\Domain\Math\GPS::create(-29.7121501, -51.1192845);
+        $y = \App\Domain\Math\GPS::create(-29.7059957, -51.1224042);
+
+        $this->assertEquals(\App\Domain\Math\Geolocator::create($x, $y)->arc2dist(), 766.72);
     }
 }
