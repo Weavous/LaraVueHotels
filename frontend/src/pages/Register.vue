371813db6cd3d90359832988a3e048c3e18f3bfd
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 22:38:43 -0300

    Register.vue

--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -19,8 +19,25 @@
 
     <div class="row">
       <div class="col">
+        <label>Lat</label>
+        <input type="text" v-model="this.lat" class="form-control" required readonly />
+      </div>
+      <div class="col">
+        <label>Lng</label>
+        <input type="text" v-model="this.lng" class="form-control" required readonly />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
         <label>Description</label>
         <input type="text" v-model="description" class="form-control" required />
+      </div>
+    </div>
+
+    <div class="row mt-2">
+      <div @click="attrpos" class="col">
+        <div class="w-100" id="map"></div>
       </div>
     </div>
 
@@ -47,6 +64,9 @@ export default {
       price: "",
       address_id: 0,
       loaded: false,
+      map: {},
+      lat: 0,
+      lng: 0,
     };
   },
   components: { Addresses },
@@ -64,23 +84,25 @@ export default {
         this.title.price > 0 ||
         this.title.address_id > 0
       ) {
+        const data = {
+          title: this.title,
+          description: this.description,
+          price: this.price,
+          address_id: this.address_id,
+          lat: this.lat,
+          lng: this.lng,
+        };
+
         if (this.id > 0) {
-          properties.update({
-            id: this.id,
-            title: this.title,
-            description: this.description,
-            price: this.price,
-            address_id: this.address_id,
-          });
+          properties.update(this.id, data);
         } else {
-            properties.store({
-            title: this.title,
-            description: this.description,
-            price: this.price,
-            address_id: this.address_id,
-          });
+          properties.store(data);
         }
       }
+    },
+    attrpos() {
+      this.lat = this.map.center.lat();
+      this.lng = this.map.center.lng();
     },
     change(id) {
       this.address_id = id;
@@ -95,13 +117,20 @@ export default {
         this.description = data.data.description;
         this.price = data.data.price;
         this.address_id = data.data.address_id;
+        this.lat = data.data.lat;
+        this.lng = data.data.lng;
       });
     }
 
     this.loaded = true;
+
+    this.map = new google.maps.Map(document.getElementById("map"), { center: new google.maps.LatLng(-29, -49), zoom: 6, mapTypeId: google.maps.MapTypeId.ROADMAP });
   },
 };
 </script>
 
 <style>
+#map {
+  height: 50vh;
+}
 </style>
