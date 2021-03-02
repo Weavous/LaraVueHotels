e699ce4d3ca4061ace4bce71629295ff744c2cab
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-02 13:32:34 -0300

    About.vue

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>About</h1>
+  <div class="container">
+    <div>
+      <div class="col">
+        <div class="masthead-followup-icon d-inline-block mb-2 text-white bd-bg-purple-bright"></div>
+        <h2 class="display-5 fw-normal">Lorem Ipsum</h2>
+        <p class="lead fw-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed odio a odio laoreet posuere ac vitae nisi.</p>
+        <p>Pellentesque nec euismod enim, vel viverra ante. Sed sit amet ornare sem. Vestibulum eu ante pharetra, hendrerit odio in, elementum lacus. Nunc id dictum quam. Vivamus quam turpis, consectetur sit amet egestas eu, elementum efficitur quam. Donec tellus quam, viverra gravida fermentum suscipit, sodales sed eros.</p>
+        <a href="https://icons.getbootstrap.com/" class="btn btn-lg btn-outline-primary mb-3">Get Bootstrap Icons</a>
+      </div>
+    </div>
   </div>
 </template>
 
