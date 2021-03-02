e258a982811447c869d4fd96f660db4f4d155e73
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-02 12:17:23 -0300

    PropertySeeder.php

--- backend/database/seeders/PropertySeeder.php
+++ backend/database/seeders/PropertySeeder.php
@@ -13,13 +13,24 @@ class PropertySeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 32; $i++) {
+        for ($i = 0; $i < 16; $i++) {
             \App\Models\Property::create([
                 "title" => \Faker\Factory::create()->text(32),
                 "description" => \Faker\Factory::create()->text(64),
                 "price" => rand(10000000, 100000000),
                 "lat" => rand(0, 1800) / 10,
                 "lng" => rand(0, 1800) / 10,
+                "address_id" => \App\Models\Address::inRandomOrder()->get()->first()->id
+            ]);
+        }
+
+        for ($i = 0; $i < 16; $i++) {
+            \App\Models\Property::create([
+                "title" => \Faker\Factory::create()->text(32),
+                "description" => \Faker\Factory::create()->text(64),
+                "price" => rand(10000000, 100000000),
+                "lat" => rand(10000, 10001) / 100,
+                "lng" => rand(10000, 10001) / 100,
                 "address_id" => \App\Models\Address::inRandomOrder()->get()->first()->id
             ]);
         }
