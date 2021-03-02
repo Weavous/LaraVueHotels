261b5dcf9b6cf71892f77126bd476ad73e3d681d
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-02 13:23:53 -0300

    NearProperty.vue

---
+++ frontend/src/components/NearProperty.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row">
+    <div v-for="(property, key) in this.data" :key="key" class="col-md-3 mt-2">
+      <Card :data="{ property }"></Card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "../components/Card.vue";
+
+import properties from "../services/properties.js";
+
+export default {
+  name: "NearProperty",
+  components: { Card },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  props: {
+    lat: {
+      required: true,
+      default: 0,
+    },
+    lng: {
+      required: true,
+      default: 0,
+    },
+    id: {
+      required: true,
+      default: 0,
+    },
+  },
+  mounted() {
+    const res = properties.nearestproperties(this.lat.lat, this.lng.lng);
+
+    res.then((response) => {
+      this.data = response.data.filter((n) => {
+        return parseInt(n.id) !== parseInt(this.id.id);
+      });
+    });
+  },
+};
+</script>
+
+<style>
+</style>
