2540d72834cbb6e1e424d84f201dc3ee1550971f
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-01 22:41:05 -0300

    2021_02_27_193930_create_addresses_table.php

---
+++ backend/database/migrations/2021_02_27_193930_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string("street", 64)->nullable(false);
+            $table->string("city", 64)->nullable(false);
+            $table->string("state", 64)->nullable(false);
+            $table->string("code", 64)->nullable(false);
+            $table->string("country", 64)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
