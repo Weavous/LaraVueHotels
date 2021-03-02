20c65ca3570b6a89a4320ab871f2c462a7d2304a
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-02 14:14:23 -0300

    properties.js

--- frontend/src/services/properties.js
+++ frontend/src/services/properties.js
@@ -5,5 +5,6 @@ export default {
     store: (data) => { return http.post(`/properties`, data) },
     show: (id) => { return http.get(`/properties/${id}`) },
     update: (id, data) => { return http.put(`/properties/${id}`, data) },
+    delete: (id) => { return http.delete(`/properties/${id}`) },
     nearestproperties: (lat, lng) => { return http.get(`/nearestproperties/${lat}/${lng}`) }
 }
