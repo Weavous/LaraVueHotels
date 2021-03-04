55878989dd341aa603421808847db0f1bb9642b6
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-04 07:28:13 -0300

    GeographicCoordinateTest.php

--- backend/tests/Unit/GeographicCoordinateTest.php
+++ backend/tests/Unit/GeographicCoordinateTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
@@ -13,7 +15,7 @@ class GeographicCoordinateTest extends TestCase
      */
     public function test_lat()
     {
-        $lat = \App\Domain\Math\GeographicCoordinate::create("-29.7123751");
+        $lat = \App\Domain\Math\GeographicCoordinate::create(-29.7123751);
 
         $this->assertEquals($lat->degrees(), -29);
         $this->assertEquals($lat->minutes(), -42);
@@ -27,7 +29,7 @@ class GeographicCoordinateTest extends TestCase
      */
     public function test_lng()
     {
-        $lng = \App\Domain\Math\GeographicCoordinate::create("-51.1191654");
+        $lng = \App\Domain\Math\GeographicCoordinate::create(-51.1191654);
 
         $this->assertEquals($lng->degrees(), -51);
         $this->assertEquals($lng->minutes(), -7);
