b1b33f90208e6e9dd44c216a5745e493bb4d2a12
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-03-04 08:54:13 -0300

    CarouselUnit.vue

---
+++ frontend/src/components/CarouselUnit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="carousel-item" v-bind:class="{ active: active }">
+    <img :src="this.src" class="d-block w-100" style="height: 600px; width: 1200px" alt="#" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselUnit",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  }
+};
+</script>
+
+<style>
+</style>
